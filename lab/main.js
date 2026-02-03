26a4903e89e835b22c01247363acf58fe8d5b599
lucaswgoncalves <lucas.wendell.98@gmail.com>
2026-02-03 11:11:04 -0300

    feat: created first lab class

---
+++ lab/main.js
@@ -0,0 +1,11 @@
+let fullName = "crew de jesus";
+
+function displayName(fullName) {
+    let arrayString = fullName.split(" ");
+    let name = arrayString[0];
+    let lastName = arrayString[arrayString.length - 1];
+
+    return `Hello! ${name} ${lastName}!`;
+}
+
+console.log(displayName(fullName));
