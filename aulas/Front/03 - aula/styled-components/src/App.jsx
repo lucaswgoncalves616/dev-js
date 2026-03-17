d10eb7cf817d97592194b37edfe11f332e42bf2e
lucaswgoncalves <lucas.wendell.98@gmail.com>
2026-03-17 10:22:19 -0300

    feat: added components

---
+++ aulas/Front/03 - aula/styled-components/src/App.jsx
@@ -0,0 +1,16 @@
+
+// import primeiroButao from "CSSModules/button.jsx";
+import Button from "./CSSModules/button.jsx";
+import SecButton from "./CSSModules/SecButton.jsx";
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Oi</h1>
+            <Button />
+            <SecButton />
+        </div>
+
+    );
+}
+
---
+++ aulas/Front/03 - aula/styled-components/src/StyledComponents/aside.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const AsideContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding: 10%;
+    background: cornflowerblue;
+    `;
+
+export default function Aside() {
+    return (
+        <aside>
+            <AsideContainer>
+                <p>asdaosdkaosd</p>
+            </AsideContainer>
+        </aside>
+    )
+}
