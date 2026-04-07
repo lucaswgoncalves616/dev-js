daa470c40959d7d7c184dbd955b569815a234d0e
lucaswgoncalves616 <lucas.wendell.98@gmail.com>
2026-04-07 10:02:26 -0300

    feat: create header

---
+++ aulas/Front/rickAndMorty-API/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Header } from "./components/Header/index.jsx";
+
+function App() {
+
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
+export default App
